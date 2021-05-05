e549d90c823dc0e96c706f4bc3fa69ad3292e552
PhoenixAki <seanstan95@gmail.com>
2021-05-05 05:28:25 -0400

    About Page (final) - Simplified text and adjusted minor styling issues on the About page.

--- front/About.tsx
+++ front/About.tsx
@@ -17,7 +17,8 @@ const styles = {
     padding: 25px;
   `,
   pageList: css`
-    padding-left: 25px;
+    padding-left: 27px;
+    padding-top: 20px;
   `,
 };
 
@@ -27,10 +28,11 @@ export default function About() {
     <div className={styles.container(bodyTextColor)}>
       <strong className={styles.heading}>About this Website</strong>
       <span className={styles.text}>
-        I created this website as my Capstone project for my Master's Degree program at Bridgewater
-        State University in the Spring 2021 semester. The goal of my project was to train an AI
-        model to function as a chatbot, and implement it into a website. This website is the
-        culmination of that project, and I'm very proud of how it turned out.
+        I created this website for my Capstone project (master's degree) in Spring 2021 at
+        Bridgewater State University. The goal of was to train an AI model to function as a chatbot,
+        and implement it into a website. This website is the culmination of that project, and I'm
+        very proud of how it turned out.
+        <br />
         <br />
         There are 3 pages accessible on this website, below is a detailed description of what each
         one does:
@@ -38,33 +40,28 @@ export default function About() {
           <li>Interact</li>
           <span className={styles.text}>
             The Interact page is where you can directly interact with the chatbot. Type messages
-            into the message input area at the bottom of your browser and hit enter to send them to
-            the server. The AI will think and send its reply. Due to limitations on processing
-            power, the AI may take up to 10-15 seconds to reply.
+            into the message input area and hit enter to send them to the server. The AI will think
+            and send its reply. Due to limitations on processing power, the AI may take up to 10-15
+            seconds to reply.
           </span>
           <li>Settings</li>
           <span className={styles.text}>
             The Settings page is where you can change a variety of settings. Most of the settings
             are colors for various elements across the website. You can also change the username
-            that is used when messages are sent, as well as what font should be used. In both cases,
-            simply type in the username or font style, and the setting will update in real time (you
-            do not need to hit enter for them to take effect). As a result of this, if an invalid
-            font name is currently in the text box, your browser will revert to whatever its default
-            font is.
+            that is used when messages are sent, as well as what font should be used. You do not
+            need to hit enter or refresh for the settings to take effect.
             <br />
             <br />
             Also included is a setting called "Server Echo Reply". If this setting is checked, the
-            server will instead respond with a copy of what you just sent it. This was mostly used
-            for testing purposes, to make sure the server is working without having to wait for the
-            AI to generate a reply. I left the setting in for you to use if wanted, the website will
-            still function normally if left ignored.
+            server will instead respond with a copy of what you just sent it. This can be used for
+            testing purposes, to make sure the server is working without having to wait for an AI
+            response.
           </span>
           <li>About</li>
           <span className={styles.text}>
-            The About page (where you currently are!) details what this website is and what purposes
-            it serves, as well as details of how each individual page works. Also included is a
-            section describing the tools and resources I used to make this project, so that you can
-            get started doing it yourself.
+            The About page (where you currently are!) details what this website is, as well as how
+            each individual page works. Also included is a section describing the tools and
+            resources I used to make this project, so that you can get started doing it yourself.
           </span>
         </ul>
       </span>
@@ -80,10 +77,10 @@ export default function About() {
         that it can be as clear as possible what each function and file is providing to the project.
         <br />
         <br />
-        Note that the only parts of this section you need in order to mess with the AI yourself is
-        the AI Training section. A website is NOT needed in order to use the AI model, as the
-        website is only a way of visually representing it replies. Behind the scenes, it is a Python
-        script that the AI uses to generate its reply, using the{" "}
+        Note that the only parts of this section you need in order to interact with your own AI is
+        the AI Training section. A website is NOT needed in order to use an AI model, as the website
+        is only a way of visually representing it. Behind the scenes, it is a Python script that the
+        AI uses to generate its reply, using the{" "}
         <a href="https://github.com/minimaxir/gpt-2-simple" target="_blank">
           gpt-2-simple library
         </a>
@@ -96,8 +93,9 @@ export default function About() {
           GPT2, released by OpenAI
         </a>
         . GPT2 is designed to predict the next element in a sequence. That sequence can be anything,
-        but here it was trained to predict the next word when given a sequence of words. To train
-        the model, I used{" "}
+        but here it was trained to predict the next word, given another sequence of words.
+        Effectively, it learned how to speak English, and will respond in English to messages. To
+        train an AI, you need a sample dataset. To do this, I used{" "}
         <a href="https://praw.readthedocs.io/en/latest/" target="_blank">
           PRAW
         </a>{" "}
@@ -106,7 +104,7 @@ export default function About() {
           r/CasualConversation
         </a>{" "}
         subreddit. Once the comments were downloaded (and after processing the data a bit) the AI
-        model was trained using the comments as sample data using{" "}
+        model was trained using the comments{" "}
         <a
           href="https://colab.research.google.com/drive/1VLG8e7YSEwypxU-noRNhsv5dW4NfTGce"
           target="_blank">
